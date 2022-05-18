d18678fdf095d6f62cc525561d0db0f13f6d1ebb
nelliott82 <nikkoelliott@gmail.com>
2022-05-18 19:48:22 -0400

    Changed some of the comments for future TODOs after reading more

--- client/store/store.js
+++ client/store/store.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/main.js';
-// will probably want to import API information like key to load data initially
 
 const initialState = {
-  //TODO
+  //TODO: key/value pairs with appropriate names and data types (empty)
 };
 
 var store = createStore(
