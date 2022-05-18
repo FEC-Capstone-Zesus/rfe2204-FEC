112c376ee18b7f3052640c8229befb4e4ca1da38
nelliott82 <nikkoelliott@gmail.com>
2022-05-18 19:48:52 -0400

    Created reducers dir with main.js rootReducer. To be filled in later.

---
+++ client/reducers/main.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// Need to import reducers once written
+
+var rootReducer = combineReducers({
+  // TODO: Add reducers as key/value pairs
+})
+
+export default rootReducer;
