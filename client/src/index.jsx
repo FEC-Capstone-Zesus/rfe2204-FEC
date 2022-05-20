3e1457f8fb71ac739a7f9f1c6430ca95aff87878
YK Dong <ykdong1991@gmail.com>
2022-05-20 15:15:49 -0400

    create global style

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
-import store from "../store/store.js"; 
+import store from "../store/store.js";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: white;
+    font-family: 'Inter', sans-serif;
+  }
+  h1 {
+    color: blue;
+  }
+  `;
 
 const App = () => {
-  return (<h1>Hello World!</h1>);
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World!</h1>
+    </>
+  );
 };
 
 render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
