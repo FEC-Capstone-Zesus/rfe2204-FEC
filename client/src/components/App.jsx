520b4e4e19239f3873f9123ed7a6e77aaf8113ee
nelliott82 <nikkoelliott@gmail.com>
2022-05-23 18:53:59 -0400

    Added some global styling and a header. Still need more global styling.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,10 @@
 import styled, { createGlobalStyle } from "styled-components";
+import OverviewContainer from "../containers/overview/OverviewContainer.js";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    margin: 0;
+    margin-left: 12rem;
+    margin-right 12rem;
     padding: 0;
     background: white;
     font-family: 'Inter', sans-serif;
@@ -14,6 +16,12 @@ const GlobalStyle = createGlobalStyle`
     color: black;
   }
   `;
+
+const SubOverview = styled.div`
+  margin-left: 7rem;
+  margin-right 7rem;
+  padding: 0;
+`;
 
 const Modal = styled.div`
   z-index: auto;
@@ -34,23 +42,52 @@ const Container = styled.div`
   transform: translate(-50%, -50%);
 `;
 
+const Header = styled.div`
+  background: rgba(226,226,226,100);
+  height: 5rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SearchBar = styled.div`
+  border: 1px solid rgba(0, 0, 0, 100);
+  width: 9rem;
+  display: inline-block;
+`
+
 const App = ( { loading, product, reviews, styles, metaData, questions, relatedProducts } ) => {
 
   return (
     <>
       <GlobalStyle />
-      <Modal loading={loading} >
-        <Container>
-          <h1 className='loading' >Loading...</h1>
-        </Container>
-      </Modal>
-      <h1>Hello World!!!!!</h1>
-      {product.id ? <p>{ JSON.stringify(product)}</p> : null}
-      {reviews.product ? <p>{ JSON.stringify(reviews)}</p> : null}
-      {styles.product_id ? <p>{ JSON.stringify(styles)}</p> : null}
-      {metaData.product_id ? <p>{ JSON.stringify(metaData)}</p> : null}
-      {questions.length ? <p>{ JSON.stringify(questions)}</p> : null}
-      {relatedProducts.length ? <p>{ JSON.stringify(relatedProducts)}</p> : null}
+        <Modal loading={loading} >
+          <Container>
+            <h1 className='loading' >Loading...</h1>
+          </Container>
+        </Modal>
+        <Header>
+          <img style={{ marginTop: 1.3 + 'rem',
+                        marginLeft: 0.5 + 'rem',
+                        height: 3 + 'rem',
+                        width: 7.5 + 'rem' }}src='/assets/Zeus_Logo.png'></img>
+          <div>
+            <SearchBar />
+            <img style={{ marginTop: 2 + 'rem',
+                          marginRight: 0.5 + 'rem',
+                          height: 1.5 + 'rem',
+                          width: 1.5 + 'rem' }}src='/assets/magnifying_glass.png'></img>
+          </div>
+        </Header>
+        <h1>Hello World!!!!!</h1>
+        <OverviewContainer />
+        <SubOverview>
+          {product.id ? <p>{ JSON.stringify(product)}</p> : null}
+          {reviews.product ? <p>{ JSON.stringify(reviews)}</p> : null}
+          {styles.product_id ? <p>{ JSON.stringify(styles)}</p> : null}
+          {metaData.product_id ? <p>{ JSON.stringify(metaData)}</p> : null}
+          {questions.length ? <p>{ JSON.stringify(questions)}</p> : null}
+          {relatedProducts.length ? <p>{ JSON.stringify(relatedProducts)}</p> : null}
+        </SubOverview>
     </>
   );
 };
