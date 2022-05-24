6148e93611485d8ffa10845e6eb5125eea920f8e
nelliott82 <nikkoelliott@gmail.com>
2022-05-24 16:49:56 -0300

    Added global styling for direction arrows. Simply add className "arrow [direction]" to use.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -26,6 +26,28 @@ const GlobalStyle = createGlobalStyle`
     background: white;
     height: 2rem;
   }
+  .arrow {
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+  }
+  .right {
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+  }
+  .left {
+    transform: rotate(135deg);
+    -webkit-transform: rotate(135deg);
+  }
+  .up {
+    transform: rotate(-135deg);
+    -webkit-transform: rotate(-135deg);
+  }
+  .down {
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+  }
 `;
 
 const SubOverview = styled.div`
@@ -63,6 +85,7 @@ const Header = styled.div`
 const SearchBar = styled.div`
   border: 1px solid rgba(0, 0, 0, 100);
   width: 9rem;
+  display: inline-block;
 `
 
 const App = ( { loading, product, reviews, styles, metaData, questions, relatedProducts } ) => {
