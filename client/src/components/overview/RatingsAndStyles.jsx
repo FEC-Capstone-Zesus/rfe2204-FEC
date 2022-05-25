f96f3dd22f6aa27cf3a93343a562c32cd1ec9a76
nelliott82 <nikkoelliott@gmail.com>
2022-05-25 17:02:02 -0300

    Started some of the functionality and HTML for the size and qty selector.

--- client/src/components/overview/RatingsAndStyles.jsx
+++ client/src/components/overview/RatingsAndStyles.jsx
@@ -7,9 +7,7 @@ const RatingsStyles = styled.div`
   padding: 2rem 1.5rem 2rem 1.5rem;
 `
 
-var start = 0;
-var end = 7;
-var imageIndex = 0;
+var totalQty = 0;
 
 const RatingsAndStyles = ( { product, styles, currentStyle, reviews, changeCurrentStyle } ) => {
 
@@ -19,6 +17,16 @@ const RatingsAndStyles = ( { product, styles, currentStyle, reviews, changeCurre
     var max = styles.results[0].photos.length;
   }
 
+  if (currentStyle.style_id) {
+    var skusArray = Object.entries(currentStyle.skus).sort((a, b) => a[1].size - b[1].size);
+
+    totalQty = skusArray.reduce((total, sku) => {
+      return total + sku;
+    }, 0)
+    var [skus, changeSkus] = useState(skusArray);
+    var [qty, changeQty] = useState(0);
+  }
+
   var averageRating = 0;
   if (reviews.product_id) {
     averageRating = reviews.ratings.reduce((total, rating) => {
@@ -26,7 +34,7 @@ const RatingsAndStyles = ( { product, styles, currentStyle, reviews, changeCurre
     }, 0) / reviews.ratings.length;
   }
 
-  const updateCurrentImage = (photo) => {
+  const updateCurrentStyle = (style) => {
     if (photo.thumbnail_url !== currentImage) {
       styles.results[0].photos.forEach((currentPhoto, i) => {
         if (currentPhoto.thumbnail_url === photo.thumbnail_url) {
@@ -35,6 +43,12 @@ const RatingsAndStyles = ( { product, styles, currentStyle, reviews, changeCurre
       })
       changeImage(photo.thumbnail_url);
     }
+  }
+
+  const changeSelect = (e) => {
+    e.preventDefault();
+
+
   }
 
   return (
@@ -55,7 +69,7 @@ const RatingsAndStyles = ( { product, styles, currentStyle, reviews, changeCurre
       <h3>{product.id ? product.category.toUpperCase() : null}</h3>
       <h1>{product.id ? product.name : null}</h1>
       &nbsp;
-      {currentStyle ? !currentStyle.sale_price ?
+      {currentStyle.style_id ? !currentStyle.sale_price ?
         <p>${currentStyle.original_price}</p> :
         <>
           <p style={{ color: 'red' }}>${currentStyle.sale_price}</p>
@@ -68,11 +82,20 @@ const RatingsAndStyles = ( { product, styles, currentStyle, reviews, changeCurre
       <div className='styles-container'>
         <h3 style={{ fontWeight: 'bolder' }}>STYLE > {currentStyle.style_id ?
             <p style={{ fontWeight: 'lighter' }}>{currentStyle.name.toUpperCase()}</p> : null}</h3>
-        <img></img>
+        {styles.product_id ? styles.results.map(style =>
+        <img key={style.style_id} src={style.photos[0].thumbnail_url} ></img>) : null}
       </div>
       <div className='selector-container'>
-        <select name='size'></select>
-        <select name='qty'></select>
+        <select name='size' onChange={(e) => changeSelect(e)}>
+          <option value='Select Size' >Select Size</option>
+          {currentStyle.style_id ? skusArray.map(sku =>
+          <option key={sku[0]} value={skus[0] + ' ' + sku[1].size} >{sku[1].size}</option>) : null}
+        </select>
+        <select name='qty' onChange={(e) => changeSelect(e)}>
+          <option value='Select Qty' >Select Qty</option>
+          {currentStyle.style_id ? styles.results.map((style, i) =>
+          <option key={style.style_id} value={i + 1} >{i + 1}</option>) : null}
+        </select>
       </div>
       <button style={{ width: 10 + 'rem', fontWeight: 500 }}>ADD TO CART</button>
       <div className='star-item'></div>
