cbf1580e3aca22a2b67c2b896cd1d81aa5116502
nelliott82 <nikkoelliott@gmail.com>
2022-05-26 19:47:59 -0300

    Made changes to what props and what actions are mapped to props.

--- client/src/containers/overview/ImageGalleryContainer.js
+++ client/src/containers/overview/ImageGalleryContainer.js
@@ -1,13 +1,24 @@
 import { connect } from "react-redux";
 import ImageGallery from "../../components/overview/ImageGallery.jsx";
+import setMainImage from "../../../actions/setMainImage.js"
 
 const mapStateToProps = (state) => (
   {
     currentStyle: state.currentStyle,
-    currentStyleId: state.currentStyleId
+    mainImage: state.mainImage,
+    imagesArray: state.imagesArray,
+    startStore: state.startStore,
+    endStore: state.endStore
   }
 );
 
-const ImageGalleryContainer = connect(mapStateToProps)(ImageGallery);
+
+var mapDispatchToProps = (dispatch) => {
+  return {
+    changeMainImage: (mainImage) => dispatch(setMainImage(mainImage))
+  };
+};
+
+const ImageGalleryContainer = connect(mapStateToProps, mapDispatchToProps)(ImageGallery);
 
 export default ImageGalleryContainer;
