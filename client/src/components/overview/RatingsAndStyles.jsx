7dca61dab63c3305be01024fb96ae555bad2f6d4
nelliott82 <nikkoelliott@gmail.com>
2022-05-28 17:18:07 -0300

    Changed StarRating to be universal component.

--- client/src/components/overview/RatingsAndStyles.jsx
+++ client/src/components/overview/RatingsAndStyles.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from "styled-components";
+import StarRating from '../StarRating.jsx'
 
 const RatingsStyles = styled.div`
   width: 25rem;
@@ -81,18 +82,6 @@ const RatingsAndStyles = ( { product,
     var [inStock, setInStock] = totalQty ? useState(true) : useState(false);
   }
 
-  var averageRating = 0;
-  var starFraction = 0;
-  if (metaData.product_id) {
-    var ratings = Object.entries(metaData.ratings);
-    var total = 0;
-    averageRating = ratings.reduce((stars, rating) => {
-      total = total + parseInt(rating[1]);
-      return stars + (rating[0] * rating[1]);
-    }, 0) / total;
-    starFraction = (Math.round((averageRating % 1) * 4) / 4) * 100;
-  }
-
   const updateCurrentStyle = (style) => {
     if (style.style_id !== currentStyle.style_id) {
       changeCurrentStyle(style);
@@ -151,15 +140,7 @@ const RatingsAndStyles = ( { product,
       <div className='ratings'>
         <span>
           {metaData.product_id ?
-            <>
-              <span>{('★').repeat(Math.floor(averageRating))}</span>
-              <span style={{ position: 'absolute' }}>
-                <StarFraction starFraction={starFraction}>
-                  <span>★</span>
-                </StarFraction>
-              </span>
-              <span>{('☆').repeat(5 - Math.floor(averageRating))}</span>
-            </>
+            <StarRating metaData={metaData} />
           : null}
         </span>
         &nbsp;
