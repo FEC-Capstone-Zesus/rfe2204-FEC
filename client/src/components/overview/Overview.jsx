4ccd4503cb789378ed701427ba2c122b872fc97e
nelliott82 <nikkoelliott@gmail.com>
2022-06-01 18:04:44 -0400

    Added ShareButtons component and removed unecessary code

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -5,15 +5,9 @@ import RatingsAndStyles from './RatingsAndStyles.jsx';
 import ImageGalleryContainer from '../../containers/overview/ImageGalleryContainer.js';
 import RatingsAndStylesContainer from '../../containers/overview/RatingsAndStylesContainer.js';
 import ExpandedViewContainer from '../../containers/overview/ExpandedViewContainer.js';
+import ShareButtons from './ShareButtons.jsx';
 
 
-const OverviewDIV = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100vw;
-`
 const ImagesStylesContainer = styled.div`
   display: flex;
 `
@@ -45,6 +39,7 @@ const Overview = ( { expanded, toggleExpanded } ) => {
           </>
         }
       </ImagesStylesContainer>
+      <ShareButtons/>
     </>
   );
 };
