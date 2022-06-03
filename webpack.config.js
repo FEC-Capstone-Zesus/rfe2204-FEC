486fac0e70d2c5d0f5d34ff5e9ad4addf255fa18
nelliott82 <nikkoelliott@gmail.com>
2022-06-03 14:36:04 -0400

    Final styling changes to star rating for better display of fractions

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const path = require("path");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: path.join(__dirname, "/client/src/index.jsx"),
   output: {
     path: path.join(__dirname, "/client/dist"),
